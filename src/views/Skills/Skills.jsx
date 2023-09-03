f398ad6b7da2699d804719577d787717b9564d21
martinvidela <videla116@gmail.com>
2023-09-03 18:57:36 -0300

    'skills'

--- src/views/Skills/Skills.jsx
+++ src/views/Skills/Skills.jsx
@@ -8,17 +8,35 @@ export const Skills = () => {
       <h2 align='center'>Frontend</h2>
       <div className="skills-icons">
         <p align="center">
-          <a href="https://skillicons.dev">
-            <img className="skills-img" src="https://skillicons.dev/icons?i=html,css,bootstrap,sass,js,vite,react,styledcomponents,redux" />
-          </a>
+          <img src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white" alt="" />
+          <img src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white" alt="" />
+          <img src="https://img.shields.io/badge/bootstrap-%238511FA.svg?style=for-the-badge&logo=bootstrap&logoColor=white" alt="" />
+          <img src="https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white" alt="" />
+          <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E" alt="" />
+          <img src="https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white" alt="" />
+          <img src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB" alt="" />
+          <img src="https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white" alt="" />
+          <img src="https://img.shields.io/badge/redux-%23593d88.svg?style=for-the-badge&logo=redux&logoColor=white" alt="" />
+          <img src="https://img.shields.io/badge/styled--components-DB7093?style=for-the-badge&logo=styled-components&logoColor=white" alt="" />
+          <img src="https://img.shields.io/badge/yarn-%232C8EBB.svg?style=for-the-badge&logo=yarn&logoColor=white" alt="" />
         </p>
       </div>
       <h2 style={{marginTop:'10px'}} align="center">Backend</h2>
       <div className="skills-icons">
         <p align="center">
-          <a href="https://skillicons.dev">
-            <img className="skills-img" src="https://skillicons.dev/icons?i=nodejs,express,firebase,mongodb" />
-          </a>
+         <img src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white" alt="" />
+         <img src="https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB" alt="" />
+         <img src="https://img.shields.io/badge/firebase-%23039BE5.svg?style=for-the-badge&logo=firebase" alt="" />
+         <img src="https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white" alt="" />
+         <img src="https://img.shields.io/badge/Socket.io-black?style=for-the-badge&logo=socket.io&badgeColor=010101" alt="" />
+        </p>
+      </div>
+      <h2 style={{marginTop:'10px'}} align="center">Extra</h2>
+      <div className="skills-icons">
+      <p align="center">
+        <img src="https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white" alt="" />
+        <img src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white" alt="" />
+        <img src="https://img.shields.io/badge/gitlab-%23181717.svg?style=for-the-badge&logo=gitlab&logoColor=white" alt="" />
         </p>
       </div>
     </div>
