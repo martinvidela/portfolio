3397ec16f54622327b7f4d618e9906f982d8e163
martinvidela <videla116@gmail.com>
2024-02-10 18:50:48 -0300

    expadded

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./Nav.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 export const Nav = () => {
+  const navigate = useNavigate();
+
+  const handleSkillsClick = () => {
+    // Redirige primero al inicio y luego a habilidades después de un pequeño retraso
+    navigate("/");
+    setTimeout(() => {
+      document.getElementById("skills").scrollIntoView({ behavior: "smooth" });
+    }, 500); // Cambia este valor si necesitas ajustar la velocidad de desplazamiento
+  };
   return (
     <div className="container-nav">
       <div className="me">
@@ -9,12 +18,21 @@ export const Nav = () => {
           <h3>Yo</h3>
         </Link>
       </div>
-      <div className="technologies">
-      </div>
+      <div className="technologies"></div>
       <div className="projects">
-        <Link to={'/projects'} className="controls">
+        <Link to={"/projects"} className="controls">
           <h3>Proyectos</h3>
         </Link>
+      </div>
+      <div className="projects">
+        <a
+          style={{ cursor: "pointer" }}
+          href="#skills"
+          onClick={handleSkillsClick}
+          className="controls"
+        >
+          <h3>Habilidades</h3>
+        </a>
       </div>
     </div>
   );
--- src/views/Experience/Experience.jsx
+++ src/views/Experience/Experience.jsx
@@ -3,20 +3,43 @@ export const Experience = () => {
   return (
     <div className="container-experience">
       <h1 className="title-experience">Experiencia Laboral</h1>
-      <div>
+      <div style={{padding:'10px'}}>
         <div>
-          <h3 className="subtitle-experience">
-            Frontend Developer <small>(UltraCore)</small>
-          </h3>
+          <div className="subtitle-container">
+            <h3 className="subtitle-experience">
+              Frontend Developer{" "}
+              <small className="ultracore">(UltraCore)</small>
+            </h3>
+            <h3 style={{ fontWeight: "lighter" }}>08/2023 - Actualidad</h3>
+          </div>
           <ul className="list-experience">
             <li>
-              Desarollo de sitios web utilizando React | Redux | Custom Hooks |
-              Tailwind CSS | Clean Architecture | Wordpress
+              Desarollo de sitios web utilizando React | React Native | Redux |
+              Custom Hooks | Tailwind CSS | Wordpress | NextJS
             </li>
             <li>
               Dentro de las mismas me encargo de la estética y arquitectura, y
               de refactorizar con el fin de mejorar la funcionalidad de los
               sitios.
+            </li>
+            <li>Trabajamos con metodología Scrum</li>
+          </ul>
+        </div>
+        <div style={{ marginTop: "50px" }}>
+          <div className="subtitle-container">
+            <h3 className="subtitle-experience">
+              Frontend Developer <small className="ultracore">(SPA)</small>
+            </h3>
+            <h3 style={{ fontWeight: "lighter" }}>06/2023 - 07/2023</h3>
+          </div>
+          <ul className="list-experience">
+            <li>
+              Desarollador de una SPA (single page application) utilizando React
+              | TailwindCSS
+            </li>
+            <li>
+              Implemente las conexiones al Backend utilizando Axios para
+              conectarme a la base de datos.
             </li>
             <li>Trabajamos con metodología Scrum</li>
           </ul>
